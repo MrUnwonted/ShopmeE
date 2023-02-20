2605c91bd8d26ad166ead6fe42d410668437a45c
MrUnwonted <arjungodzarjun@gmail.com>
2023-02-20 18:54:43 +0530

    new form added

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryController.java
@@ -28,84 +28,88 @@ public class CategoryController {
 		return  "categories/categories";
 	}
 	
+	
 	@GetMapping("/categories/new")
-	public String newUser(Model model) {
-		Category category = new Category();
-		category.setEnabled(true);
-		model.addAttribute("category", category);
-		model.addAttribute("pageTitle", "Create new category");
+	public String newCategory(Model model) {
+//		Category category = new Category();
+//		List<Category> listCategories = service.listCategoriesUsedInForm();
+//		category.setEnabled(true);
+		model.addAttribute("category", new Category());
+//		model.addAttribute("listCategories", listCategories);
+//		model.addAttribute("pageTitle", "Create New Category");
+		
 		return "categories/categoryform";
 	}
 	
-	@PostMapping("/categories/save")
-	public String saveUser(Category category, RedirectAttributes redirectAttributes)  {
-		
-//		if (!multipartFile.isEmpty()) {
-//			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-////			category.setPhotos(fileName);
-//			Category savedcategory = service.save(category);
-//			
-//			String uploadDir = "category-photos/" + savedcategory.getId();
-//			
-//			FileUploadUtil.cleanDir(uploadDir);
-//			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
-//			
-//		} else {
-////			if (category.getPhotos().isEmpty()) category.setPhotos(null);
-//			
+//	@PostMapping("/categories/save")
+//	public String saveCategory(Category category, RedirectAttributes redirectAttributes)  {
+//		
+////		if (!multipartFile.isEmpty()) {
+////			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+//////			category.setPhotos(fileName);
+////			Category savedcategory = service.save(category);
+////			
+////			String uploadDir = "category-photos/" + savedcategory.getId();
+////			
+////			FileUploadUtil.cleanDir(uploadDir);
+////			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+////			
+////		} else {
+//////			if (category.getPhotos().isEmpty()) category.setPhotos(null);
+////			
+////		}
+//		service.save(category);
+//		
+//		redirectAttributes.addFlashAttribute("message", "The category has been saved successfully.");
+//		
+//		return "redirect:/categories";
+//	}
+//	
+//	@GetMapping("/categories/edit/{id}")
+//	public String editcategory(@PathVariable(name = "id") Integer id,
+//			Model model,
+//			RedirectAttributes redirectAttributes) {
+//		try {
+//			Category category = service.get(id);
+//			model.addAttribute("category", category);
+//			model.addAttribute("pageTitle", "Edit category (ID: " +id+ " )");
+//			return "categoryform";
+//		}catch (Exception e) {
+//			redirectAttributes.addFlashAttribute("message", e.getMessage());	
+//			return "redirect:/categories";
 //		}
-		service.save(category);
-		
-		redirectAttributes.addFlashAttribute("message", "The category has been saved successfully.");
-		
-		return "redirect:/categories";
-	}
-	
-	@GetMapping("/categories/edit/{id}")
-	public String editcategory(@PathVariable(name = "id") Integer id,
-			Model model,
-			RedirectAttributes redirectAttributes) {
-		try {
-			Category category = service.get(id);
-			model.addAttribute("category", category);
-			model.addAttribute("pageTitle", "Edit category (ID: " +id+ " )");
-			return "categoryform";
-		}catch (Exception e) {
-			redirectAttributes.addFlashAttribute("message", e.getMessage());	
-			return "redirect:/categories";
-		}
-		
-		
-	}
-	
-	
-	@GetMapping("/categories/delete/{id}")
-	public String deletecategory(@PathVariable(name = "id") Integer id, 
-			Model model,
-			RedirectAttributes redirectAttributes) {
-		try {
-			service.delete(id);
-//			String categoryPhotosDir = "category-photos/" + id;
-			
-			redirectAttributes.addFlashAttribute("message", 
-					"The category ID " + id + " has been deleted successfully");
-		} catch (Exception ex) {
-			redirectAttributes.addFlashAttribute("message", ex.getMessage());
-		}
-		
-		return "redirect:/categories";
-	}
-	
-	
-	@GetMapping("/categories/{id}/enabled/{status}")
-	public String updatecategoryEnabledStatus(@PathVariable("id") Integer id,
-			@PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
-				service.updateCategoryEnabledStatus(id, enabled);
-				String status = enabled ? "enabled" : "disabled";
-				String message =  "The category ID" + id + " has been " + status;
-				redirectAttributes.addFlashAttribute("message", message);
-						
-		return "redirect:/categories";
-	}
+//		
+//		
+//	}
+//	
+//	
+//	@GetMapping("/categories/delete/{id}")
+//	public String deletecategory(@PathVariable(name = "id") Integer id, 
+//			Model model,
+//			RedirectAttributes redirectAttributes) {
+//		try {
+//			service.delete(id);
+////			String categoryPhotosDir = "category-photos/" + id;
+//			
+//			redirectAttributes.addFlashAttribute("message", 
+//					"The category ID " + id + " has been deleted successfully");
+//		} catch (Exception ex) {
+//			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+//		}
+//		
+//		return "redirect:/categories";
+//	}
+//	
+//	
+//	@GetMapping("/categories/{id}/enabled/{status}")
+//	public String updatecategoryEnabledStatus(@PathVariable("id") Integer id,
+//			@PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
+//				service.updateCategoryEnabledStatus(id, enabled);
+//				String status = enabled ? "enabled" : "disabled";
+//				String message =  "The category ID" + id + " has been " + status;
+//				redirectAttributes.addFlashAttribute("message", message);
+//						
+//		return "redirect:/categories";
+//	}
 	
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -1,8 +1,10 @@
 package com.shopme.admin.category;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import org.hibernate.mapping.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,60 +15,91 @@ import com.shopme.common.entity.Category;
 public class CategoryService {
 
 	@Autowired
-	private CategoryRepository catRepo;
+	private CategoryRepository repo;
 	
 	
 	public List<Category> listAll(){
-		return (List<Category>) catRepo.findAll();
+		return (List<Category>) repo.findAll();
 	}
 	
-	public Category save(Category cat) {
-		boolean isUpdatingCategory = (cat.getId() != null) ; 
+	
+	
+	public List<Category> listCategoriesUsedInForm() {
+		List<Category> categoriesUsedInForm = new ArrayList<>();
 		
-		if(isUpdatingCategory) {
-			Category existingCategory= catRepo.findById(cat.getId()).get(); 
-		}	
+		Iterable<Category> categoriesInDB = repo.findAll();
 		
-		return catRepo.save(cat);
+//		for (Category category : categoriesInDB) {
+//			if (category.getParent() == null) {
+//				categoriesUsedInForm.add(new Category(category.getName()));
+//				
+//				Set<Category> children = category.getChildren();
+//				
+//				for (Category subCategory : children) {
+//					String name = "--" + subCategory.getName();
+//					categoriesUsedInForm.add(new Category(name));
+//					
+//					listChildren(categoriesUsedInForm, subCategory, 1);
+//				}
+//			}
+//		}		
+		
+		return categoriesUsedInForm;
 	}
 	
-	public boolean isCategoryUnique(Integer id, String name){
-		Category catByAlias =catRepo.getCatByName(name);
-		
-		if(catByAlias == null) return true;
-		boolean isCreatingNew = (id == null);
-		
-		if(isCreatingNew) {
-				if(catByAlias != null ) return false;
-		} else {
-			if (catByAlias .getId() != id) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
 	
-	public Category get(Integer id) throws UserNotFoundException {
-		
-		try {
-		return catRepo.findById(id).get();
-		} catch (NoSuchElementException ex) {
-			throw new UserNotFoundException("Could not find any Category with ID" + id);
-		}
-	}
 	
-	public void delete(Integer id) throws UserNotFoundException {
-		Long countById = catRepo.countById(id);
-		if (countById == null || countById == 0) {
-			throw new UserNotFoundException("Could not find any Category with ID" + id);
-		}
-		catRepo.deleteById(id);
-	}
 	
-	public void updateCategoryEnabledStatus(Integer id, boolean enabled) {
-		catRepo.updateEnabledStatus(id, enabled);
-	}
+	
+	
+	
+//	public Category save(Category cat) {
+//		boolean isUpdatingCategory = (cat.getId() != null) ; 
+//		
+//		if(isUpdatingCategory) {
+//			Category existingCategory= catRepo.findById(cat.getId()).get(); 
+//		}	
+//		
+//		return catRepo.save(cat);
+//	}
+//	
+//	public boolean isCategoryUnique(Integer id, String name){
+//		Category catByAlias =catRepo.getCatByName(name);
+//		
+//		if(catByAlias == null) return true;
+//		boolean isCreatingNew = (id == null);
+//		
+//		if(isCreatingNew) {
+//				if(catByAlias != null ) return false;
+//		} else {
+//			if (catByAlias .getId() != id) {
+//				return false;
+//			}
+//		}
+//		
+//		return true;
+//	}
+//	
+//	public Category get(Integer id) throws UserNotFoundException {
+//		
+//		try {
+//		return catRepo.findById(id).get();
+//		} catch (NoSuchElementException ex) {
+//			throw new UserNotFoundException("Could not find any Category with ID" + id);
+//		}
+//	}
+//	
+//	public void delete(Integer id) throws UserNotFoundException {
+//		Long countById = catRepo.countById(id);
+//		if (countById == null || countById == 0) {
+//			throw new UserNotFoundException("Could not find any Category with ID" + id);
+//		}
+//		catRepo.deleteById(id);
+//	}
+//	
+//	public void updateCategoryEnabledStatus(Integer id, boolean enabled) {
+//		catRepo.updateEnabledStatus(id, enabled);
+//	}
 
 	
 }
