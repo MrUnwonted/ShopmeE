c72d093cc7ffd1971a124c92ad520f0c02d08a07
MrUnwonted <arjungodzarjun@gmail.com>
2023-03-09 15:29:47 +0530

    Shopme Frontend working successfully

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -32,6 +32,9 @@ public class Category {
 
 	private boolean enabled;
 
+	@Column(name = "all_parent_ids", length = 256, nullable = true)
+	private String allParentIDs;
+
 	@OneToOne
 	@JoinColumn(name = "parent_id")
 	private Category parent;
@@ -177,5 +180,14 @@ public class Category {
 	public String toString() {
 		return this.name;
 	}
+
+	public String getAllParentIDs() {
+		return allParentIDs;
+	}
+
+	public void setAllParentIDs(String allParentIDs) {
+		this.allParentIDs = allParentIDs;
+	}
+
 
 }
