8ad053b5f7ead45f6e944e00268d08bbbd68b64f
MrUnwonted <arjungodzarjun@gmail.com>
2023-03-11 22:15:02 +0530

    Country added

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/product/ProductController.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/product/ProductController.java
@@ -18,94 +18,94 @@ import com.shopme.common.exception.ProductNotFoundException;
 
 @Controller
 public class ProductController {
-	@Autowired private ProductService productService;
-	@Autowired private CategoryService categoryService;
+    @Autowired private ProductService productService;
+    @Autowired private CategoryService categoryService;
 
-	@GetMapping("/c/{category_alias}")
-	public String viewCategoryFirstPage(@PathVariable("category_alias") String alias,
-			Model model) {
-		return viewCategoryByPage(alias, 1, model);
-	}
-	
-	@GetMapping("/c/{category_alias}/page/{pageNum}")
-	public String viewCategoryByPage(@PathVariable("category_alias") String alias,
-			@PathVariable("pageNum") int pageNum,
-			Model model) {
-		try {
-			Category category = categoryService.getCategory(alias);		
-			List<Category> listCategoryParents = categoryService.getCategoryParents(category);
-			
-			Page<Product> pageProducts = productService.listByCategory(pageNum, category.getId());
-			List<Product> listProducts = pageProducts.getContent();
-			
-			long startCount = (pageNum - 1) * ProductService.PRODUCTS_PER_PAGE + 1;
-			long endCount = startCount + ProductService.PRODUCTS_PER_PAGE - 1;
-			if (endCount > pageProducts.getTotalElements()) {
-				endCount = pageProducts.getTotalElements();
-			}
-			
-			
-			model.addAttribute("currentPage", pageNum);
-			model.addAttribute("totalPages", pageProducts.getTotalPages());
-			model.addAttribute("startCount", startCount);
-			model.addAttribute("endCount", endCount);
-			model.addAttribute("totalItems", pageProducts.getTotalElements());
-			model.addAttribute("pageTitle", category.getName());
-			model.addAttribute("listCategoryParents", listCategoryParents);
-			model.addAttribute("listProducts", listProducts);
-			model.addAttribute("category", category);
-			
-			return "product/products_by_category";
-		} catch (CategoryNotFoundException ex) {
-			return "error/404";
-		}
-	}
-	
-	@GetMapping("/p/{product_alias}")
-	public String viewProductDetail(@PathVariable("product_alias") String alias, Model model) {
-		
-		try {
-			Product product = productService.getProduct(alias);
-			List<Category> listCategoryParents = categoryService.getCategoryParents(product.getCategory());
-			
-			model.addAttribute("listCategoryParents", listCategoryParents);
-			model.addAttribute("product", product);
-			model.addAttribute("pageTitle", product.getShortName());
-			
-			return "product/product_detail";
-		} catch (ProductNotFoundException e) {
-			return "error/404";
-		}
-	}
-	
-	@GetMapping("/search")
-	public String searchFirstPage(@Param("keyword") String keyword, Model model) {
-		return searchByPage(keyword, 1, model);
-	}
-	
-	@GetMapping("/search/page/{pageNum}")
-	public String searchByPage(@Param("keyword") String keyword,
-			@PathVariable("pageNum") int pageNum,
-			Model model) {
-		Page<Product> pageProducts = productService.search(keyword, pageNum);
-		List<Product> listResult = pageProducts.getContent();
-		
-		long startCount = (pageNum - 1) * ProductService.SEARCH_RESULTS_PER_PAGE + 1;
-		long endCount = startCount + ProductService.SEARCH_RESULTS_PER_PAGE - 1;
-		if (endCount > pageProducts.getTotalElements()) {
-			endCount = pageProducts.getTotalElements();
-		}
-		
-		model.addAttribute("currentPage", pageNum);
-		model.addAttribute("totalPages", pageProducts.getTotalPages());
-		model.addAttribute("startCount", startCount);
-		model.addAttribute("endCount", endCount);
-		model.addAttribute("totalItems", pageProducts.getTotalElements());
-		model.addAttribute("pageTitle", keyword + " - Search Result");
-		
-		model.addAttribute("keyword", keyword);
-		model.addAttribute("listResult", listResult);
-		
-		return "product/search_result";
-	}
+    @GetMapping("/c/{category_alias}")
+    public String viewCategoryFirstPage(@PathVariable("category_alias") String alias,
+                                        Model model) {
+        return viewCategoryByPage(alias, 1, model);
+    }
+
+    @GetMapping("/c/{category_alias}/page/{pageNum}")
+    public String viewCategoryByPage(@PathVariable("category_alias") String alias,
+                                     @PathVariable("pageNum") int pageNum,
+                                     Model model) {
+        try {
+            Category category = categoryService.getCategory(alias);
+            List<Category> listCategoryParents = categoryService.getCategoryParents(category);
+
+            Page<Product> pageProducts = productService.listByCategory(pageNum, category.getId());
+            List<Product> listProducts = pageProducts.getContent();
+
+            long startCount = (pageNum - 1) * ProductService.PRODUCTS_PER_PAGE + 1;
+            long endCount = startCount + ProductService.PRODUCTS_PER_PAGE - 1;
+            if (endCount > pageProducts.getTotalElements()) {
+                endCount = pageProducts.getTotalElements();
+            }
+
+
+            model.addAttribute("currentPage", pageNum);
+            model.addAttribute("totalPages", pageProducts.getTotalPages());
+            model.addAttribute("startCount", startCount);
+            model.addAttribute("endCount", endCount);
+            model.addAttribute("totalItems", pageProducts.getTotalElements());
+            model.addAttribute("pageTitle", category.getName());
+            model.addAttribute("listCategoryParents", listCategoryParents);
+            model.addAttribute("listProducts", listProducts);
+            model.addAttribute("category", category);
+
+            return "product/products_by_category";
+        } catch (CategoryNotFoundException ex) {
+            return "error/404";
+        }
+    }
+
+    @GetMapping("/p/{product_alias}")
+    public String viewProductDetail(@PathVariable("product_alias") String alias, Model model) {
+
+        try {
+            Product product = productService.getProduct(alias);
+            List<Category> listCategoryParents = categoryService.getCategoryParents(product.getCategory());
+
+            model.addAttribute("listCategoryParents", listCategoryParents);
+            model.addAttribute("product", product);
+            model.addAttribute("pageTitle", product.getShortName());
+
+            return "product/product_detail";
+        } catch (ProductNotFoundException e) {
+            return "error/404";
+        }
+    }
+
+    @GetMapping("/search")
+    public String searchFirstPage(@Param("keyword") String keyword, Model model) {
+        return searchByPage(keyword, 1, model);
+    }
+
+    @GetMapping("/search/page/{pageNum}")
+    public String searchByPage(@Param("keyword") String keyword,
+                               @PathVariable("pageNum") int pageNum,
+                               Model model) {
+        Page<Product> pageProducts = productService.search(keyword, pageNum);
+        List<Product> listResult = pageProducts.getContent();
+
+        long startCount = (pageNum - 1) * ProductService.SEARCH_RESULTS_PER_PAGE + 1;
+        long endCount = startCount + ProductService.SEARCH_RESULTS_PER_PAGE - 1;
+        if (endCount > pageProducts.getTotalElements()) {
+            endCount = pageProducts.getTotalElements();
+        }
+
+        model.addAttribute("currentPage", pageNum);
+        model.addAttribute("totalPages", pageProducts.getTotalPages());
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
+        model.addAttribute("totalItems", pageProducts.getTotalElements());
+        model.addAttribute("pageTitle", keyword + " - Search Result");
+
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("listResult", listResult);
+
+        return "product/search_result";
+    }
 }
--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/product/ProductService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/product/ProductService.java
@@ -11,30 +11,30 @@ import com.shopme.common.exception.ProductNotFoundException;
 
 @Service
 public class ProductService {
-	public static final int PRODUCTS_PER_PAGE = 10;
-	public static final int SEARCH_RESULTS_PER_PAGE = 10;
-	
-	@Autowired private ProductRepository repo;
-	
-	public Page<Product> listByCategory(int pageNum, Integer categoryId) {
-		String categoryIdMatch = "-" + String.valueOf(categoryId) + "-";
-		Pageable pageable = PageRequest.of(pageNum - 1, PRODUCTS_PER_PAGE);
-		
-		return repo.listByCategory(categoryId, categoryIdMatch, pageable);
-	}
-	
-	public Product getProduct(String alias) throws ProductNotFoundException {
-		Product product = repo.findByAlias(alias);
-		if (product == null) {
-			throw new ProductNotFoundException("Could not find any product with alias " + alias);
-		}
-		
-		return product;
-	}
-	
-	public Page<Product> search(String keyword, int pageNum) {
-		Pageable pageable = PageRequest.of(pageNum - 1, SEARCH_RESULTS_PER_PAGE);
-		return repo.search(keyword, pageable);
-		
-	}
+    public static final int PRODUCTS_PER_PAGE = 10;
+    public static final int SEARCH_RESULTS_PER_PAGE = 10;
+
+    @Autowired private ProductRepository repo;
+
+    public Page<Product> listByCategory(int pageNum, Integer categoryId) {
+        String categoryIdMatch = "-" + String.valueOf(categoryId) + "-";
+        Pageable pageable = PageRequest.of(pageNum - 1, PRODUCTS_PER_PAGE);
+
+        return repo.listByCategory(categoryId, categoryIdMatch, pageable);
+    }
+
+    public Product getProduct(String alias) throws ProductNotFoundException {
+        Product product = repo.findByAlias(alias);
+        if (product == null) {
+            throw new ProductNotFoundException("Could not find any product with alias " + alias);
+        }
+
+        return product;
+    }
+
+    public Page<Product> search(String keyword, int pageNum) {
+        Pageable pageable = PageRequest.of(pageNum - 1, SEARCH_RESULTS_PER_PAGE);
+        return repo.search(keyword, pageable);
+
+    }
 }
