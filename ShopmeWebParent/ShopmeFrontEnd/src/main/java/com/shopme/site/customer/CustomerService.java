a3c9e09b110e267624e2d4168f55696d1915eb83
MrUnwonted <arjungodzarjun@gmail.com>
2023-03-13 22:51:58 +0530

    Customer sevice added

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/customer/CustomerService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/customer/CustomerService.java
@@ -1,19 +1,27 @@
 package com.shopme.site.customer;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Country;
 import com.shopme.common.entity.Customer;
 import com.shopme.site.setting.CountryRepository;
 
+import net.bytebuddy.utility.RandomString;
+
 @Service
+@Transactional
 public class CustomerService {
 
 	@Autowired private CountryRepository countryRepo;
 	@Autowired private CustomerRepository customerRepo;
+	@Autowired PasswordEncoder passwordEncoder;
 	
 	public List<Country> listAllCountries() {
 		return countryRepo.findAllByOrderByNameAsc();
@@ -22,5 +30,33 @@ public class CustomerService {
 	public boolean isEmailUnique(String email) {
 		Customer customer = customerRepo.findByEmail(email);
 		return customer == null;
+	}
+	
+	public void registerCustomer(Customer customer) {
+		encodePassword(customer);
+		customer.setEnabled(false);
+		customer.setCreatedTime(new Date());
+		
+		String randomCode = RandomString.make(64);
+		customer.setVerificationCode(randomCode);
+		
+		customerRepo.save(customer);
+		
+	}
+
+	private void encodePassword(Customer customer) {
+		String encodedPassword = passwordEncoder.encode(customer.getPassword());
+		customer.setPassword(encodedPassword);
+	}
+	
+	public boolean verify(String verificationCode) {
+		Customer customer = customerRepo.findByVerificationCode(verificationCode);
+		
+		if (customer == null || customer.isEnabled()) {
+			return false;
+		} else {
+			customerRepo.enable(customer.getId());
+			return true;
+		}
 	}
 }
